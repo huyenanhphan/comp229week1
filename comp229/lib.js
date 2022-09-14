8c408914871319ce0a48e2b71eca4ad075d5302f
huyenanhphan <molyhuyenanh03@gmail.com>
2022-09-14 18:55:26 -0400

    initial commit

---
+++ comp229/lib.js
@@ -0,0 +1,6 @@
+export function halfof(x){
+    return x/2;
+}
+export function multiply(x, y){
+    return x * y;
+}
---
+++ comp229/main.js
@@ -0,0 +1,3 @@
+import { halfof, multiply } from "./lib.js";
+console.log(halfof(84));
+console.log(multiply(21, 2));
